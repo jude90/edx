fa2923e0b2f1407cec7f94a798c506ec5c114e97
jude90 <442119071@qq.com>
2014-11-26 21:55:34 +0800

    fflodr

--- fp101/high_order.hs
+++ fp101/high_order.hs
@@ -2,5 +2,15 @@ module Main where
 
 main = putStrLn "Hello World"
 
+ffoldr :: (a -> b -> b) -> b -> [a] -> b
+ffoldr f v [] = v
+ffoldr f v (x:xs) = f x $ ffoldr f v xs
 twice :: (a -> a) -> a -> a
 twice f = f.f
+
+len :: [a] -> Int
+len [] = 0
+len (_:xs) = 1 + len xs 
+
+llen :: [a] -> Int
+llen  = ffoldr (\ _ x -> x+1) 0 
